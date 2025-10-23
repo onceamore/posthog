b3aa57e8031dfd6e8526a50260da41485f713a43
jovan sakovic <jovan@posthog.com>
2025-10-23 22:36:58 +0100

    feat: add project secret api keys

--- products/endpoints/backend/models.py
+++ products/endpoints/backend/models.py
@@ -68,6 +68,7 @@ class Endpoint(CreatedMetaFields, UpdatedMetaFields, UUIDTModel):
     )
 
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -76,7 +77,7 @@ class Endpoint(CreatedMetaFields, UpdatedMetaFields, UUIDTModel):
             models.UniqueConstraint(
                 fields=["team", "name"],
                 name="unique_team_endpoint_name",
-            )
+            ),
         ]
         indexes = [
             models.Index(fields=["team", "is_active"]),
